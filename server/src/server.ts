cb452812a9b14aed4e7e749c3be72211134b5fcf
RogerioVM <rogerio1597@gmail.com>
2020-08-12 21:08:44 -0300

    Finalizando projeto

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.json());
+
+app.use(routes);
+
+
+
+
+app.listen(3333,() => {
+    console.log('Ouvindo porta da NLW, 3333');
+});
---
+++ web/README.md
@@ -0,0 +1,2 @@
+# nlw-02-proffy
+Projeto da segunda edição do NLW#02, onde foi construida uma aplicação do zero utilizando React com Typescript, React Native e Node.
